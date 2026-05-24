a72532e0839341c538580cd59619ed4a046e0ad0
Deelaka Ransilu <88099140+deelaka-ransilu@users.noreply.github.com>
2026-05-24 07:14:25 +0530

    fix: update base path for custom domai

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/portfolio/", // <-- update to your current repo name
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
